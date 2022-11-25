c848d51e15473802adb291ee4034122d2e5e810f
Henrik Meyer <henrik.meyer1998@gmx.de>
2022-11-25 14:43:00 +0100

    Simple pick and place works

--- Client.py
+++ Client.py
@@ -8,14 +8,22 @@ node_id1 = "ns=2;s=test"
 node_id_start = "ns=2;s=start"
 node_id_isBusy = "ns=2;s=isBusy"
 
+# Pick and Place Positions
+
 node_id_posPick = ["ns=2;s=posXpick", "ns=2;s=posYpick", "ns=2;s=posZpick",
                    "ns=2;s=rotXpick", "ns=2;s=rotYpick", "ns=2;s=rotZpick"]
 
-pos_pick = [-0.111, -0.448, 0.088,
-            3.114, -0.264, -0.085]
-# 0.36, 3.15, -0.04
+pos_pick = [-0.090, -0.598, 0.228,
+            0.049, -2.230, 2.228]
 
-async def read_var(client, node_id):
+node_id_posPlace = ["ns=2;s=posXplace", "ns=2;s=posYplace", "ns=2;s=posZplace",
+                    "ns=2;s=rotXplace", "ns=2;s=rotYplace", "ns=2;s=rotZplace"]
+
+pos_place = [-0.633, -0.331, 0.220,
+             1.349, 2.504, -2.595]
+
+
+async def read_var_cont(client, node_id):
     while True:
         node = client.get_node(node_id)
         value = await node.read_value()
@@ -45,15 +53,22 @@ async def read_pos(client, node_ids):
 async def main():
     async with Client(url=urlUR5) as client:
 
-        read2 = asyncio.create_task(read_var(client, node_id_isBusy))
+        read_is_busy = asyncio.create_task(read_var_cont(client, node_id_isBusy))
 
+        # Define Position Pick
         await asyncio.create_task(write_pos(client, node_id_posPick, pos_pick))
         await asyncio.create_task(read_pos(client, node_id_posPick))
+
+        # Define Position Place
+        await asyncio.create_task(write_pos(client, node_id_posPlace, pos_place))
+        await asyncio.create_task(read_pos(client, node_id_posPlace))
+
+        # Start of Program
         await asyncio.create_task(start_program(client, True))
 
         await asyncio.sleep(20)
 
-        read2.cancel()
+        read_is_busy.cancel()
         print("All tasks done")
 
 
